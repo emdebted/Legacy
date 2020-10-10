6432d440edb8414c09fa0e0abdf3ee995b3103d2
Emily Parker <emdebted123@gmail.com>
2020-10-10 04:28:30 -0500

    Added state machine and default state, began story text

---
+++ Assets/State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject //Instead of Monobehavior, Scriptable Objects must derive from ScriptableObject
+{
+    [TextArea(20, 24)] [SerializeField] string storyText;
+}
