110a0db5a9dd0a11f17f4628f5e8c6aa9a707f9a
Emily Parker <emdebted123@gmail.com>
2020-10-10 02:54:26 -0500

    Added TextController script

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Needed for the "Text" type of variable
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; // SerializeField allows us to edit the field within the Unity inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "THIS IS A TEST";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
