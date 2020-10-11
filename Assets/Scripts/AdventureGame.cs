921aeb72ef99a240caa11505d2bfea3052af115c
Emily Parker <emdebted123@gmail.com>
2020-10-11 05:54:39 -0500

    Added game states and hierarchy, started outline

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Needed for the "Text" type of variable
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; // SerializeField allows us to edit the field within the Unity inspector
+    [SerializeField] State startingState;
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextStates[3];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
